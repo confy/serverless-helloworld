ddaed8092acdc0a633c19037434fc8d075c01378
Adrian McFarlane <confuzedfx@gmail.com>
2022-08-29 21:59:04 -0700

    lambda deployment

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+ default = "us-west-2"
+}
+
+variable "app_version" {
+    default = "1.0.0"
+}
+
+variable "stage" {
+	default = "main"
+}
+
+variable "resource_name" {
+	default = "hello"
+}   
